2614310d9a17740d9d4242d0dad472f98692c068
antontomusiak <36823849+antontomusiak@users.noreply.github.com>
2019-10-20 19:02:35 +0300

    Update time.py

--- srm144/time.py
+++ srm144/time.py
@@ -1,3 +1,27 @@
+'''
+Problem Statement
+    
+Computers tend to store dates and times as single numbers which represent the number of seconds or milliseconds since a particular date. 
+Your task in this problem is to write a method whatTime, which takes an integer, seconds, representing the number of seconds since midnight
+on some day, and returns a string formatted as "<H>:<M>:<S>". Here, <H> represents the number of complete hours since midnight, 
+<M> represents the number of complete minutes since the last complete hour ended, and <S> represents the number of seconds since the 
+last complete minute ended. Each of <H>, <M>, and <S> should be an integer, with no extra leading 0's. Thus, if seconds is 0, you should 
+return "0:0:0", while if seconds is 3661, you should return "1:1:1".
+
+Definition
+    
+Class:
+Time
+Method:
+whatTime
+Parameters:
+integer
+Returns:
+string
+Method signature:
+def whatTime(self, seconds):
+'''
+
 class Time:
 	def whatTime(self, seconds):
 		h = seconds/3600
